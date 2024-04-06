93935c902920e6e7fcbe56ada5e05853c6655761
ValentinLH <98545666+ValentinLH@users.noreply.github.com>
2024-04-05 21:42:19 -0400

    Ok app spring

--- src/fr/polytech/vgl/dao/TestSpring.java
+++ src/fr/polytech/vgl/dao/TestSpring.java
@@ -34,7 +34,7 @@ public class TestSpring implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		readCompany();
+//		readCompany();
 		
 	}
 	
--- src/fr/polytech/vgl/main/MainCentralApplication.java
+++ src/fr/polytech/vgl/main/MainCentralApplication.java
@@ -5,9 +5,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 
 import fr.polytech.vgl.centralapp.controller.CompanyListController;
+import fr.polytech.vgl.dao.*;
 import fr.polytech.vgl.dao.service.CompanyService;
 import fr.polytech.vgl.model.Company;
 import fr.polytech.vgl.serialisation.Serialisation;
@@ -17,12 +21,17 @@ import fr.polytech.vgl.serialisation.Serialisation;
  *
  */
 
-@Component
 public class MainCentralApplication {
 
-	@Autowired
-	static CompanyService companyService;
-	
+	private static ConfigurableApplicationContext springApplicationContext;
+
+	/**
+	 * @return the springApplicationContect
+	 */
+	public static ConfigurableApplicationContext getSpringApplicationContect() {
+		return springApplicationContext;
+	}
+
 	/**
 	 * Main permettant de lance la premi�re fen�tre Ce main permet de cr�er
 	 * l'entreprise ainsi que les employ�s La premi�re fen�tre correspond au menu
@@ -32,18 +41,29 @@ public class MainCentralApplication {
 	 */
 	public static void main(String[] args) {
 
-		
-		List<Company> l = companyService.getAllCompanies();
-		l.forEach(item -> System.out.println(item.getCompanyName()));
-		
-		
 		// gv.setVisible(true);
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+
+					springApplicationContext = SpringApplication.run(SpringApp.class, args);
+
+//					CompanyService companyService = springApplicationContext.getBean(CompanyService.class);
+
+					CompanyRepositoryItem cc = springApplicationContext.getBean(CompanyRepositoryItem.class);
+
+					
+//					List<Company> l = companyService.getAllCompanies();
+//					l.forEach(item -> System.out.println(item.getCompanyName()));
+//					System.out.println(l.size());
+					
+					List<Company> l = cc.findAll();
+					l.forEach(item -> System.out.println(item.getCompanyName()));
+					System.out.println(l.size());
+
 					// GiveCompanyView gv = new GiveCompanyView();
 					CompanyListController window = new CompanyListController();
-					
+
 					try {
 						List<Company> deSerialize = Serialisation.deserialize("company.sav");
 						// listCompany = deSerialize;
@@ -52,10 +72,10 @@ public class MainCentralApplication {
 							window.addCompany(newcomp);
 						}
 					} catch (Exception e) {
-						
+
 					}
-					
-					//window.addCompany(StubMain.stubCompany());
+
+					// window.addCompany(StubMain.stubCompany());
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
